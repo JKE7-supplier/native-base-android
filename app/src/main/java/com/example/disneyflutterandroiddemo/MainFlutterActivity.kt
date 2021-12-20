a7a7d8239d4f1c3153938d4f8712a2b4728fbdd7
Jack Ke <Jack.KE.-ND@disney.com>
2021-12-20 16:45:39 +0800

    创建项目

---
+++ app/src/main/java/com/example/disneyflutterandroiddemo/MainFlutterActivity.kt
@@ -0,0 +1,16 @@
+package com.example.disneyflutterandroiddemo
+
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+
+/**
+ * Created by Jack Ke on 2021/12/9
+ */
+class MainFlutterActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
